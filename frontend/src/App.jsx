76215db79a67eeab899f4f18e7e3098c7b3b1b9f
jishnu2021 <jishnughosh98@gmail.com>
2024-07-14 21:15:40 +0530

    user authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
-
-import './App.css';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <>
+      Hello World
+    </>
+  )
+}
+
+export default Register
